873e054fff272bfd293bffc80b05d5f354443733
hayanesuru <hayanesuru@outlook.jp>
2025-08-06 05:24:57 +0900

    cleanup

---
+++ leaf-server/src/main/java/org/dreeam/leaf/util/BlockUtil.java
@@ -0,0 +1,9 @@
+package org.dreeam.leaf.util;
+
+public class BlockUtil {
+    public static final byte WALL_TAG = 1;
+    public static final byte FENCE_TAG = 2;
+    public static final byte CLIMBABLE = 4;
+    public static final byte FENCE_GATE = 8;
+    public static final byte TRAP_DOOR = 16;
+}
